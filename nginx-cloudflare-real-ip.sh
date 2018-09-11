dffddfec6c9233df3b85c1ae454b7b369c3d79fa
VirtuBox <contact@virtubox.net>
2018-09-11 23:13:04 +0200

    fix ufw path

--- nginx-cloudflare-real-ip.sh
+++ nginx-cloudflare-real-ip.sh
@@ -8,14 +8,14 @@ IPV6=$(curl -q https://www.cloudflare.com/ips-v6)
 echo '' >/etc/nginx/conf.d/cloudflare.conf
 for cfip in $IPV4; do
     echo "set_real_ip_from $cfip;" >>/etc/nginx/conf.d/cloudflare.conf
-    if [ -x /usr/bin/ufw ]; then
+    if [ -x /usr/sbin/ufw ]; then
         ufw allow from $cfip to any port 80
         ufw allow from $cfip to any port 443
     fi
 done
 for cfip in $IPV6; do
     echo "set_real_ip_from $cfip;" >>/etc/nginx/conf.d/cloudflare.conf
-    if [ -x /usr/bin/ufw ]; then
+    if [ -x /usr/sbin/ufw ]; then
         ufw allow from $cfip to any port 80
         ufw allow from $cfip to any port 443
     fi

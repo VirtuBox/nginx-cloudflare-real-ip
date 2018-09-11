551b3bda51858f084f6316896ddc9da1a68d9e76
VirtuBox <contact@virtubox.net>
2018-09-11 23:11:29 +0200

    add script

---
+++ nginx-cloudflare-real-ip.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Simple bash script to restore visitor real IP under Cloudflare with Nginx
+# Script also whitelist cloudflare IP with UFW (if installed)
+
+IPV4=$(curl -q https://www.cloudflare.com/ips-v4)
+IPV6=$(curl -q https://www.cloudflare.com/ips-v6)
+
+echo '' >/etc/nginx/conf.d/cloudflare.conf
+for cfip in $IPV4; do
+    echo "set_real_ip_from $cfip;" >>/etc/nginx/conf.d/cloudflare.conf
+    if [ -x /usr/bin/ufw ]; then
+        ufw allow from $cfip to any port 80
+        ufw allow from $cfip to any port 443
+    fi
+done
+for cfip in $IPV6; do
+    echo "set_real_ip_from $cfip;" >>/etc/nginx/conf.d/cloudflare.conf
+    if [ -x /usr/bin/ufw ]; then
+        ufw allow from $cfip to any port 80
+        ufw allow from $cfip to any port 443
+    fi
+done
+
+echo 'real_ip_header CF-Connecting-IP;' >>/etc/nginx/conf.d/cloudflare.conf

677ec30b179268018e23b1dea27ee875345f62d8
VirtuBox <contact@virtubox.net>
2018-09-14 16:22:01 +0200

    update script & create README.md
    * fix curl error 
    * check nginx configuration & reload nginx

--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,29 @@
 # nginx-cloudflare-real-ip
+
+Configure Nginx to restore Visitor real IP under Cloudflare CDN
+
+## Features
+
+* Get Cloudflare IPv4 + IPv6 list and create nginx configuration to restore visitors real IP in `/etc/nginx/conf.d/cloudflare.conf`
+* If UFW is installed, whitelist Cloudflare IPs on port 80 & 443
+
+## Requirements
+
+* Nginx built with http_realip_module
+
+You can check if http_realip_module available with :
+
+```bash
+nginx -V 2>&1 | grep with-http_realip_module
+```
+
+If the previous command return nothing, http_realip_module isn't available
+
+---
+
+## Usage
+
+```bash
+bash <(wget -O - https://raw.githubusercontent.com/VirtuBox/nginx-cloudflare-real-ip/master/nginx-cloudflare-real-ip.sh)
+```
+
--- nginx-cloudflare-real-ip.sh
+++ nginx-cloudflare-real-ip.sh
@@ -2,27 +2,58 @@
 # Simple bash script to restore visitor real IP under Cloudflare with Nginx
 # Script also whitelist cloudflare IP with UFW (if installed)
 
-IPV4=$(curl -q https://www.cloudflare.com/ips-v4)
-IPV6=$(curl -q https://www.cloudflare.com/ips-v6)
+if [ ! -x /usr/bin/curl ]; then
+echo "####################################"
+echo "Installing CURL"
+echo "####################################"
+sudo apt-get update
+sudo apt-get install curl -y
+fi
+
+IPV4=$(curl -s https://www.cloudflare.com/ips-v4)
+IPV6=$(curl -s https://www.cloudflare.com/ips-v6)
 
 echo '' >/etc/nginx/conf.d/cloudflare.conf
-for cfip in $IPV4; do
-    echo "set_real_ip_from $cfip;" >>/etc/nginx/conf.d/cloudflare.conf
+echo "####################################"
+echo "Adding Cloudflare IPv4"
+echo "####################################"
+for cf_ip in $IPV4; do
+    echo "set_real_ip_from $cf_ip;" >>/etc/nginx/conf.d/cloudflare.conf
     if [ -x /usr/sbin/ufw ]; then
-        ufw allow from $cfip to any port 80
-        ufw allow from $cfip to any port 443
+        sudo ufw allow from $cf_ip to any port 80
+        sudo ufw allow from $cf_ip to any port 443
     fi
 done
-for cfip in $IPV6; do
-    echo "set_real_ip_from $cfip;" >>/etc/nginx/conf.d/cloudflare.conf
+echo "####################################"
+echo "Adding Cloudflare IPv6"
+echo "####################################"
+for cf_ip in $IPV6; do
+    echo "set_real_ip_from $cf_ip;" >>/etc/nginx/conf.d/cloudflare.conf
     if [ -x /usr/sbin/ufw ]; then
-        ufw allow from $cfip to any port 80
-        ufw allow from $cfip to any port 443
+        sudo ufw allow from $cf_ip to any port 80
+        sudo ufw allow from $cf_ip to any port 443
     fi
 done
-
 echo 'real_ip_header CF-Connecting-IP;' >>/etc/nginx/conf.d/cloudflare.conf
 
+VERIFY_NGINX_CONFIG=$(nginx -t 2>&1 | grep failed)
+echo "####################################"
+echo "Checking Nginx configuration"
+echo "####################################"
+if [ -z "$VERIFY_NGINX_CONFIG" ]; then
+echo "####################################"
+echo "Reloading Nginx"
+echo "####################################"
+sudo service nginx reload
+else
+echo "####################################"
+echo "Nginx configuration is not correct"
+echo "####################################"
+fi
+
 if [ -x /usr/sbin/ufw ]; then
-    ufw reload
+echo "####################################"
+echo "Reloading UFW"
+echo "####################################"
+   sudo ufw reload
 fi
